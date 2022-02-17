d57012c29f81b51ec79037d903c24c39af0162ec
henry <hzhang33@gmail.com>
2022-02-17 16:03:52 -0500

    cleanup

--- raw_data/raw_data_to_csv.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('./game_play_activity.csv', 'w') as out:
-	with open('./game_play_activity.json') as fp:
-		data = json.load(fp)
-		for row in data['rows']['Activity']:
-			str_row = map(str, row)
-			out.write(','.join(str_row) + '\n')
